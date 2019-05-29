cc5be92d15f3d3a7535653c68519af281034b57b
AdamGonda <adamgondagyula@gmail.com>
2019-05-29 15:19:59 +0200

    Story added make changes in Task and dummy data tasks to see if it's works

---
+++ src/components/Sprint/Task/Story.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import Style from 'styled-components'
+
+export default ({ story }) => {
+  const showNCharacters = n => {
+    return `${story.substring(0, n)}`
+  }
+
+  const charLimit = 75
+  const btnText = 'more'
+
+  return (
+    <Wrapper>
+      {story.length > charLimit ? (
+        <>
+          <p>{story.substring(0, charLimit)}<span>...</span></p>
+          <button>{btnText}</button>
+        </>
+      ) : (
+        <p>{story}</p>
+      )}
+    </Wrapper>
+  )
+}
+
+const Wrapper = Style.div`
+    width: 242px;
+    font-size: 12px;
+    text-align: center;
+    display: flex;
+    align-items: center;
+
+    :hover {
+        button {
+            background-color: rgba(0,0,0,0);
+            border: none;
+            border-radius: 15px;
+            border: 1px solid #2D4D60;
+            font-weight: 500;
+            display:inline-block;
+            height: 25px;
+            margin-left: 10px;
+        }
+
+        span {
+            display: none;
+        }
+    }
+
+    button {
+        display: none;
+    }
+    
+
+    button:focus {
+        outline: none;
+    }
+
+    button:active {
+        background-color: #2D4D60;
+        color: white;
+    }   
+    
+    span {
+        font-size: 22px;
+        font-weight: 500;
+        margin-top: -10px;
+        display: inline-block;  
+    }
+`
