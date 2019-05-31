3a50f1d74c9fb73d62c5a2679116a98de87af227
AdamGonda <adamgondagyula@gmail.com>
2019-05-31 18:19:37 +0200

    reducers added

---
+++ src/redux/reducers/project.js
@@ -0,0 +1,11 @@
+import sampleData from '../../sampleData'
+
+const initState = sampleData.projects
+
+export const project = (state = initState, action) => {
+  switch (action.type) {
+    
+    default:
+      return state
+  }
+}
