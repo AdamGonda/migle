040c64fbf0b26e36dd6c0e40a6da3eca34aed72a
AdamGonda <adamgondagyula@gmail.com>
2019-06-01 15:32:29 +0200

    prepare store for middlewares

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,17 @@
-import { createStore, combineReducers } from 'redux'
-import {locations} from './reducers/locations'
-import {projects} from './reducers/projects'
+import { compose, createStore, combineReducers, applyMiddleware } from 'redux'
+import { locations } from './reducers/locations'
+import { projects } from './reducers/projects'
 
+const rootReducer = combineReducers({ locations: locations, projects })
 
-const rootReducer = combineReducers({locations: locations,projects});
+const featureMiddleware = []
 
-export default createStore(
-  rootReducer,
+const coreMiddleware = []
+
+// compose store enhancers
+const enhancer = compose(
+  applyMiddleware(...featureMiddleware, ...coreMiddleware),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
+
+export default createStore(rootReducer, enhancer)
