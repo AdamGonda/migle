f87cc72ba32b617c19804148363041c7a7c4c64c
AdamGonda <adamgondagyula@gmail.com>
2019-06-01 19:42:45 +0200

    more reducer added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,17 @@
 import { compose, createStore, combineReducers, applyMiddleware } from 'redux'
 import { locations } from './reducers/locations'
 import { projects } from './reducers/projects'
+import { featureSets } from './reducers/featureSets'
+import { sprints } from './reducers/sprints'
+import { stories } from './reducers/stories'
 
-const rootReducer = combineReducers({ locations: locations, projects })
+
+const rootReducer = combineReducers({ locations: locations, projects, featureSets, sprints, stories})
 
 const featureMiddleware = []
 
 const coreMiddleware = []
 
-// compose store enhancers
 const enhancer = compose(
   applyMiddleware(...featureMiddleware, ...coreMiddleware),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
