18379c75fb7f0726d905a36ee30a628328b643b0
AdamGonda <adamgondagyula@gmail.com>
2019-06-01 19:51:09 +0200

    renaming folders

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
-import Home from './components/Home/Home'
-import Project from './components/Project/Project'
-import FeatureSet from './components/Feature set/FeatureSet'
-import Sprint from './components/Sprint/Sprint'
+import Home from './components/Home page/Home'
+import Project from './components/Project page/Project'
+import FeatureSet from './components/Feature set page/FeatureSet'
+import Sprint from './components/Sprint page/Sprint'
 
 export default () => {
   return (
