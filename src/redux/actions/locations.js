06d8b880a5b9eeb5f461136bbe45966835887007
AdamGonda <adamgondagyula@gmail.com>
2019-06-01 19:44:20 +0200

    location reducer added location action creator added

--- src/redux/actions/locations.js
+++ src/redux/actions/locations.js
@@ -3,12 +3,12 @@ export const NAVIGATE_TO = 'NAVIGATE_TO'
 export const NAVBAR = 'NAVBAR'
 export const LINK = 'LINK'
 
-export const navigateToLocation = (idx, to, using) => ({
+export const navigateTo = (to, using, options) => ({
   type: `${NAVIGATE_TO} [${to.name.toUpperCase()}] VIA [${using}]`,
   payload: {
-    idx,
-    name: to.name,
-    path: to.path
+    to,
+    history: options.history,
+    idx: options.idx
   }
 })
 
--- src/redux/reducers/locations.js
+++ src/redux/reducers/locations.js
@@ -4,21 +4,42 @@ import sampleData from '../../sampleData'
 const initState = sampleData.locations
 
 export const locations = (state = initState, action) => {
-  console.log('Hello state', state)
   switch (true) {
     case action.type.includes(NAVIGATE_TO):
       if (action.type.includes(NAVBAR)) {
-        return state.filter((x, idx) => {
+        return state.filter((location, idx) => {
           if (idx == 0) {
+            changeLocationUsingReactRouterHistory(action, calcPath(action))
             return idx <= action.payload.idx
           } else {
+            changeLocationUsingReactRouterHistory(action, calcPath(action))
             return idx < action.payload.idx
           }
         })
       } else if (action.type.includes(LINK)) {
-        return [...state, action.payload]
+        
+        
+        return [...state, action.payload.to]
       }
     default:
       return state
   }
 }
+
+const calcPath = action => {
+  const location = action.payload.to
+
+  if (location.path == '/') {
+    return '/'
+  } else if (location.path === 'project') {
+    return `/${location.path}/${location.id}`
+  } else if (location.path === 'feature-set') {
+    return `/project/${location.path}/${location.id}`
+  } else if (location.path === 'sprint') {
+    return `/project/feature-set/${location.path}/${location.id}`
+  } else if (location.path === 'story') {
+    return `/project/feature-set/sprint/${location.id}`
+  }
+}
+const changeLocationUsingReactRouterHistory = (action, path) =>
+  action.payload.history.push(path)
