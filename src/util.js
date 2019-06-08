d85426c22b47a51736bf4cbe2d0ba8983638679a
AdamGonda <adamgondagyula@gmail.com>
2019-06-08 09:27:03 +0200

    deleting some files

--- src/util.js
+++
@@ -1,7 +0,0 @@
-export const getIdFromEndOfPath = ownProps => {
-  let path = ownProps.location.pathname
-  let idStartIdx = path.search('[0-9]')
-  return path.slice(idStartIdx, path.length)
-}
-
-export const findProperty = (property, state, id) => state[property].find(x => x.id == id)
