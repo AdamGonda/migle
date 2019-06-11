b8ed16e1e65fcc56f1ad822dee6f043726bcb847
AdamGonda <adamgondagyula@gmail.com>
2019-06-11 13:08:36 +0200

    transform removed from animation in item

--- src/components/Module/Item.js
+++ src/components/Module/Item.js
@@ -52,12 +52,10 @@ const Wrapper = Style.div`
 
   @keyframes showUp {
     0% {
-      transform: translate(0px, -20px);
       opacity: 0;
     }
 
     100% {
-      transform: translate(0px, 0px);
       opacity: 1;
     }
   }
