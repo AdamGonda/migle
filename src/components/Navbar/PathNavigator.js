5ef263c70cb631cfb62a9432423fe1e0e0013c51
AdamGonda <adamgondagyula@gmail.com>
2019-06-11 12:40:14 +0200

    bugfix: key warning

--- src/components/Navbar/PathNavigator.js
+++ src/components/Navbar/PathNavigator.js
@@ -20,7 +20,7 @@ const PathNavigator = ({ history, locations, navigateTo }) => {
       )
     } else {
       return (
-        <>
+        <React.Fragment key={idx}>
           <div
             onClick={() => {
               navigateTo(location, NAVBAR, { idx: idx + 1, history })
@@ -29,7 +29,7 @@ const PathNavigator = ({ history, locations, navigateTo }) => {
             {location.name}
           </div>
           {idx < locations.length - 1 ? <span>|</span> : null}
-        </>
+        </React.Fragment>
       )
     }
   }
