3d4d1cf43601ee93bbd523f3826cd1e6716c0416
AdamGonda <adamgondagyula@gmail.com>
2019-06-12 15:16:43 +0200

    SignUp modeule and Login module replaced with Auth module
    sign up action added to action creator

--- src/redux/actions/auth.js
+++ src/redux/actions/auth.js
@@ -1,8 +1,34 @@
+export const AUTH_SUCCESS = 'AUTH_SUCCESS'
+export const AUTH_ERROR = 'AUTH_ERROR'
+
 export const signIn = credentials => (dispatch, getState, { getFirebase }) => {
   const fireBase = getFirebase()
   fireBase
     .auth()
     .signInWithEmailAndPassword(credentials.email, credentials.password)
-    .then(() => console.log('happy path :)', credentials))
-    .catch(() => console.log('sad path :(', credentials))
+    .then(() => dispatch({type: AUTH_SUCCESS}))
+    .catch(err => dispatch({type: AUTH_ERROR, payload: err}))
+}
+
+export const signUp = newUser => (
+  dispatch,
+  getState,
+  { getFirebase, getFirestore }
+) => {
+  const fireBase = getFirebase()
+  const fireSore = getFirestore()
+  fireBase
+    .auth()
+    .createUserWithEmailAndPassword(newUser.email, newUser.password)
+    .then(resp => {
+      return fireSore
+        .collection('users')
+        .doc(resp.user.uid)
+        .set({
+          firstName: newUser.firstName,
+          lastName: newUser.lastName
+        })
+    })
+    .then(() => dispatch({type: AUTH_SUCCESS}))
+    .catch(err => dispatch({type: AUTH_ERROR, payload: err}))
 }
