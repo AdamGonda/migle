d8aed92590fad4e0020ff32225af916c62e421d4
AdamGonda <adamgondagyula@gmail.com>
2019-06-12 22:41:11 +0200

    bugfix/empty project name not allowed

--- src/components/Module/ActionBtn.js
+++ src/components/Module/ActionBtn.js
@@ -7,6 +7,8 @@ import CloseIcon from './assets/close icon.svg'
 const ActionBtn = ({ action }) => {
   const [isOpend, setIsOpend] = React.useState(false)
   const [name, setName] = React.useState('')
+  const [emptyNameError, setEmptyNameError] = React.useState(false)
+  const emptyNameErrorMsg = 'required'
 
   const showActionBtn = () => {
     return <Panel><MainBtn src={AddNewIcon} onClick={() => setIsOpend(true)} /></Panel>
@@ -16,16 +18,25 @@ const ActionBtn = ({ action }) => {
     return (
       <Panel>
         
-          <Input placeholder="name" onChange={e => setName(e.target.value)} />
+          <Input placeholder={emptyNameError ? emptyNameErrorMsg : 'name'} onChange={e => {
+            setEmptyNameError(false)
+            setName(e.target.value)
+          }} />
           <div>
             <SubmitBtn
               src={SubmitIcon}
               onClick={() => {
-                action(name)
-                setIsOpend(false)
+                if(!name == ''){
+                  action(name)
+                  setIsOpend(false)
+                }
+                setEmptyNameError(true)
               }}
             />
-            <CancelBtn src={CloseIcon} onClick={() => setIsOpend(false)} />
+            <CancelBtn src={CloseIcon} onClick={() => {
+              setIsOpend(false)
+              setEmptyNameError(false)
+              }} />
           </div>
           
       </Panel>
