5382dd8242969b412b8d91fe88aaaad105bb064f
AdamGonda <adamgondagyula@gmail.com>
2019-06-13 20:13:29 +0200

    bugfix: Multiple pushes to history, The effect of the last link in path navigator

--- src/redux/reducers/locations.js
+++ src/redux/reducers/locations.js
@@ -6,12 +6,11 @@ export const locations = (state = initState, action) => {
   switch (true) {
     case action.type.includes(NAVIGATE_TO):
       if (action.type.includes(NAVBAR)) {
+        pushLocationToHistory(action)
         return state.filter((location, idx) => {
           if (idx === 0) {
-            action.payload.history.push(calcPath(action))
             return idx <= action.payload.idx
           } else {
-            action.payload.history.push(calcPath(action))
             return idx < action.payload.idx
           }
         })
@@ -24,16 +23,19 @@ export const locations = (state = initState, action) => {
   }
 }
 
-const calcPath = action => {
+const pushLocationToHistory = action => {
   const location = action.payload.to
+  let pathToPush = ''
 
   if (location.type === '') {
-    return '/'
+    pathToPush = '/'
   } else if (location.type === 'personal-project') {
-    return `/${location.type}/${location.id}`
+    pathToPush = `/${location.type}/${location.id}`
   } else if (location.type === 'team-project') {
-    return `/${location.type}/${location.id}`
-  } else if (location.type === 'sprint') {
-    return `/project/${location.type}/${location.id}`
+    pathToPush = `/${location.type}/${location.id}`
+  }else if(location.type == 'sprint'){
+    return
   }
+
+  action.payload.history.push(pathToPush)
 }
